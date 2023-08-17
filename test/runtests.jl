b34b5a6ec1e408833595c462ded70904edcd914a
Daniel Stoops <danielst@omnisys.co.il>
2023-08-17 20:09:46 +0300

    Add test CZML Corridor

--- test/runtests.jl
+++ test/runtests.jl
@@ -274,6 +274,204 @@ Some of the expected results may have been minimally modifed from Cesium Sandcas
         @test expected_result == JSON.parsefile(fileName)
     end
 
+    @testset "CZML Corridor" begin
+        # https://sandcastle.cesium.com/?src=CZML%20Corridor.html&label=CZML
+        str_CZML = """[
+            {
+              id: "document",
+              name: "CZML Geometries: Polyline",
+              version: "1.0",
+            },
+            {
+              id: "redCorridor",
+              name: "Red corridor on surface with rounded corners",
+              corridor: {
+                positions: {
+                  cartographicDegrees: [
+                    -100.0,
+                    40.0,
+                    0,
+                    -105.0,
+                    40.0,
+                    0,
+                    -105.0,
+                    35.0,
+                    0,
+                  ],
+                },
+                width: 200000.0,
+                material: {
+                  solidColor: {
+                    color: {
+                      rgba: [255, 0, 0, 127],
+                    },
+                  },
+                },
+              },
+            },
+            {
+              id: "greenCorridor",
+              name: "Green corridor at height with mitered corners and outline",
+              corridor: {
+                positions: {
+                  cartographicDegrees: [
+                    -90.0,
+                    40.0,
+                    0,
+                    -95.0,
+                    40.0,
+                    0,
+                    -95.0,
+                    35.0,
+                    0,
+                  ],
+                },
+                height: 100000.0,
+                cornerType: "MITERED",
+                width: 200000.0,
+                material: {
+                  solidColor: {
+                    color: {
+                      rgba: [0, 255, 0, 255],
+                    },
+                  },
+                },
+                outline: true, // height must be set for outlines to display
+                outlineColor: {
+                  rgba: [0, 0, 0, 255],
+                },
+              },
+            },
+            {
+              id: "blueCorridor",
+              name: "Blue extruded corridor with beveled corners and outline",
+              corridor: {
+                positions: {
+                  cartographicDegrees: [
+                    -80.0,
+                    40.0,
+                    0,
+                    -85.0,
+                    40.0,
+                    0,
+                    -85.0,
+                    35.0,
+                    0,
+                  ],
+                },
+                height: 200000.0,
+                extrudedHeight: 100000.0,
+                width: 200000.0,
+                cornerType: "BEVELED",
+                material: {
+                  solidColor: {
+                    color: {
+                      rgba: [0, 0, 255, 255],
+                    },
+                  },
+                },
+                outline: true,
+                outlineColor: {
+                  rgba: [255, 255, 255, 255],
+                },
+              },
+            },
+          ]"""
+        expected_result = CZML_string_to_JSON(str_CZML)
+
+        # recreate using CZML
+        p0 = Preamble(; name = "CZML Geometries: Polyline")
+        p1 = Packet(;
+            id = "redCorridor",
+            name = "Red corridor on surface with rounded corners",
+            corridor = Corridor(;
+                positions = PositionList(;
+                    cartographicDegrees = [-100.0,
+                        40.0,
+                        0,
+                        -105.0,
+                        40.0,
+                        0,
+                        -105.0,
+                        35.0,
+                        0],
+                ),
+                width = 200000.0,
+                material = Material(;
+                    solidColor = SolidColorMaterial(;
+                        color = Color(; rgba = [255, 0, 0, 127]),
+                    ),
+                ),
+            ),
+        )
+        p2 = Packet(;
+            id = "greenCorridor",
+            name = "Green corridor at height with mitered corners and outline",
+            corridor = Corridor(;
+                positions = PositionList(;
+                    cartographicDegrees = [
+                        -90.0,
+                        40.0,
+                        0,
+                        -95.0,
+                        40.0,
+                        0,
+                        -95.0,
+                        35.0,
+                        0,
+                    ],
+                ),
+                height = 100000.0,
+                cornerType = CornerTypes.MITERED,
+                width = 200000.0,
+                material = Material(;
+                    solidColor = SolidColorMaterial(;
+                        color = Color(; rgba = [0, 255, 0, 255]),
+                    ),
+                ),
+                outline = true,
+                outlineColor = Color(; rgba = [0, 0, 0, 255]),
+            ),
+        )
+        p3 = Packet(;
+            id = "blueCorridor",
+            name = "Blue extruded corridor with beveled corners and outline",
+            corridor = Corridor(;
+                positions = PositionList(;
+                    cartographicDegrees = [
+                        -80.0,
+                        40.0,
+                        0,
+                        -85.0,
+                        40.0,
+                        0,
+                        -85.0,
+                        35.0,
+                        0,
+                    ],
+                ),
+                height = 200000.0,
+                extrudedHeight = 100000.0,
+                width = 200000.0,
+                cornerType = CornerTypes.BEVELED,
+                material = Material(;
+                    solidColor = SolidColorMaterial(;
+                        color = Color(; rgba = [0, 0, 255, 255]),
+                    ),
+                ),
+                outline = true,
+                outlineColor = Color(; rgba = [255, 255, 255, 255]),
+            ),
+        )
+        d = Document([p0, p1, p2, p3])
+        fileName = tempname() * ".czml"
+        printCZML(d, fileName)
+
+        # tests
+        @test isfile(fileName)
+        @test expected_result == JSON.parsefile(fileName)
+    end
+
     @testset "CZML Box" begin
         # https://sandcastle.cesium.com/?src=CZML%20Box.html&label=CZML
         str_CZML = """[
