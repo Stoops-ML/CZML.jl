2ef188a00c7c77d14bf59d5e1535a324c3fe52dd
Daniel Stoops <danielst@omnisys.co.il>
2023-08-19 23:25:14 +0300

    Add test CZML Spheres and Ellipsoids

--- test/runtests.jl
+++ test/runtests.jl
@@ -356,6 +356,147 @@ Some of the expected results may have been minimally modifed from Cesium Sandcas
         @test expected_result == JSON.parsefile(fileName)
     end
 
+    @testset "CZML Spheres and Ellipsoids" begin
+        str_CZML = """[
+            {
+              id: "document",
+              name: "CZML Geometries: Spheres and Ellipsoids",
+              version: "1.0",
+            },
+            {
+              id: "blueEllipsoid",
+              name: "blue ellipsoid",
+              position: {
+                cartographicDegrees: [-114.0, 40.0, 300000.0],
+              },
+              ellipsoid: {
+                radii: {
+                  cartesian: [200000.0, 200000.0, 300000.0],
+                },
+                fill: true,
+                material: {
+                  solidColor: {
+                    color: {
+                      rgba: [0, 0, 255, 255],
+                    },
+                  },
+                },
+              },
+            },
+            {
+              id: "redSphere",
+              name: "Red sphere with black outline",
+              position: {
+                cartographicDegrees: [-107.0, 40.0, 300000.0],
+              },
+              ellipsoid: {
+                radii: {
+                  cartesian: [300000.0, 300000.0, 300000.0],
+                },
+                fill: true,
+                material: {
+                  solidColor: {
+                    color: {
+                      rgba: [255, 0, 0, 100],
+                    },
+                  },
+                },
+                outline: true,
+                outlineColor: {
+                  rgbaf: [0, 0, 0, 1],
+                },
+              },
+            },
+            {
+              id: "yellowEllipsoid",
+              name: "ellipsoid with yellow outline",
+              position: {
+                cartographicDegrees: [-100.0, 40.0, 300000.0],
+              },
+              ellipsoid: {
+                radii: {
+                  cartesian: [200000.0, 200000.0, 300000.0],
+                },
+                fill: false,
+                outline: true,
+                outlineColor: {
+                  rgba: [255, 255, 0, 255],
+                },
+                slicePartitions: 24,
+                stackPartitions: 36,
+              },
+            },
+          ]"""
+        expected_result = CZML_string_to_JSON(str_CZML)
+
+        # recreate using CZML
+        p0 = Preamble(;
+            name = "CZML Geometries: Spheres and Ellipsoids",
+        )
+        p1 = Packet(;
+            id = "blueEllipsoid",
+            name = "blue ellipsoid",
+            position = Position(;
+                cartographicDegrees = [-114.0, 40.0, 300000.0],
+            ),
+            ellipsoid = Ellipsoid(;
+                radii = EllipsoidRadii(;
+                    cartesian = [200000.0, 200000.0, 300000.0],
+                ),
+                fill = true,
+                material = Material(;
+                    solidColor = SolidColorMaterial(;
+                        color = Color(;
+                            rgba = [0, 0, 255, 255],
+                        ),
+                    ),
+                ),
+            ))
+        p2 = Packet(;
+            id = "redSphere",
+            name = "Red sphere with black outline",
+            position = Position(;
+                cartographicDegrees = [-107.0, 40.0, 300000.0],
+            ),
+            ellipsoid = Ellipsoid(;
+                radii = EllipsoidRadii(;
+                    cartesian = [300000.0, 300000.0, 300000.0],
+                ),
+                fill = true,
+                material = Material(;
+                    solidColor = SolidColorMaterial(;
+                        color = Color(;
+                            rgba = [255, 0, 0, 100],
+                        ),
+                    ),
+                ),
+                outline = true,
+                outlineColor = Color(; rgbaf = [0, 0, 0, 1]),
+            ))
+        p3 = Packet(;
+            id = "yellowEllipsoid",
+            name = "ellipsoid with yellow outline",
+            position = Position(;
+                cartographicDegrees = [-100.0, 40.0, 300000.0],
+            ),
+            ellipsoid = Ellipsoid(;
+                radii = EllipsoidRadii(;
+                    cartesian = [200000.0, 200000.0, 300000.0],
+                ),
+                fill = false,
+                outline = true,
+                outlineColor = Color(; rgba = [255, 255, 0, 255]),
+                slicePartitions = 24,
+                stackPartitions = 36))
+        d = Document([p0, p1, p2, p3])
+        fileName = tempname() * ".czml"
+        printCZML(d, fileName)
+
+        # tests
+        @test isfile(fileName)
+        @test expected_result == JSON.parsefile(fileName)
+    end
+
     @testset "CZML Point" begin
         # https://sandcastle.cesium.com/?src=CZML%20Point.html&label=CZML
         str_CZML = """[
